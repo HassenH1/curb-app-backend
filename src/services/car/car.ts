c84d622bf5f274ac0b81d84a1de6c98af47e3833
HassenH1 <hasansaid51@gmail.com>
2023-01-15 17:58:32 -0800

    ...

--- src/services/car/car.ts
+++ src/services/car/car.ts
@@ -1,26 +1,58 @@
 import { Request, Response, NextFunction } from 'express'
+import User from '../../models/user/user.model'
 
 class Car {
+  getACar = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      User.findOne({ _id: req.params.userId }, (err: any, user: any) => {
+        if (err) return res.status(400).json(err)
+        const test = user.cars.id(req.params.carId)
+        return res.status(200).json(test)
+      })
+    } catch (error) {
+      throw new Error(`${error} error getting a car`)
+    }
+  }
   addCar = async (req: Request, res: Response, next: NextFunction) => {
     try {
-    } catch (error) {}
+      const data = await User.findOne({ _id: req.body._id })
+      data?.cars.push({
+        licensePlateNumber: req.body.licensePlateNumber,
+        carModel: req.body.carModel,
+        default: req.body.default || false,
+      })
+
+      const updated = await data?.save()
+      res.status(200).json(updated?.cars)
+    } catch (error) {
+      throw new Error(`${error} adding car`)
+    }
   }
   deleteCar = async (req: Request, res: Response, next: NextFunction) => {
     try {
-    } catch (error) {}
-  }
-  getCars = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-    } catch (error) {}
-  }
-  getACar = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-    } catch (error) {}
+      User.findByIdAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { cars: { _id: req.params.carId } } },
+        { safe: true, new: true },
+        (err, result) => {
+          if (err) {
+            return res.status(400).json(err)
+          }
+          return res.status(200).json(result?.cars)
+        }
+      )
+    } catch (error) {
+      throw new Error(`${error} delete car`)
+    }
   }
   updateCar = async (req: Request, res: Response, next: NextFunction) => {
     try {
     } catch (error) {}
   }
+  // getCars = async (req: Request, res: Response, next: NextFunction) => {
+  //   try {
+  //   } catch (error) {}
+  // }
 }
 
 const CarService = new Car()
